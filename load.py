647809c0344ecf73728854fd8b33dde08ff0d35e
kylemfry3 <33091901+kylemfry3@users.noreply.github.com>
2017-10-25 08:57:51 -0400

    Crayon Loader (1.x)
    Will be used in future releases in the 1.x family of Crayon programs to help load the OS.

---
+++ load.py
@@ -0,0 +1,17 @@
+#Crayon Loader v1.0 designed and compiled by Kyle Fry
+import os
+import time
+
+print("-----CRAYON v1.0 by Kyle Fry-----")
+x = input("What would you like to do? [LOAD] to start Crayon, [STOP] to quit this program: ")
+print(x)
+
+if x == "LOAD" :
+	time.sleep(3)
+	C:\Users\[User Here]\[Path Here]\crayon.html
+
+if x == "STOP" :
+	print("Stopping in 3 seconds...")
+	quit()
+else:
+	print("ERROR: Please restart program with correct arguements.")
